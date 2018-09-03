ce0a94b73fa3414f225fbbb30f97d66ec9800f5f
lscho <me@lscho.com>
2018-09-03 18:35:40 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,35 @@
+## 获取
+
+``
+git clone https://github.com/lscho/sshp
+``
+
+## 使用
+
+```
+chmod +x ./sshp.sh
+```
+
+#### 添加
+
+```
+./sshp.sh add
+```
+
+#### 查看
+
+```
+./sshp.sh ls
+```
+
+#### 删除
+
+```
+./sshp.sh rm server_name
+```
+
+#### 登陆
+
+```
+./sshp.sh server_name
+```
---
+++ sshp.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+command=$1
+path=~/.ssh/sshp/
+add(){
+    read -r -p "server name:" server_name
+    read -r -p "server host:" server_host
+    read -r -p "server port(22):" server_port
+    read -r -p "server user(root):" server_user
+    read -r -p "server password:" server_password
+
+    if [ ! $server_port ]
+    then
+    	server_port=22
+    fi
+
+    if [ !$server_user ]
+    then
+    	server_user="root"
+    fi
+
+	if [ ! -d $path ]
+	then
+		echo "mkdir $path"
+		( mkdir $path )
+	fi
+
+	if [ ! -f $path$server_name ]
+	then
+		echo "touch $path$server_name"
+		( touch $path$server_name )
+	else
+
+		read -r -p "server_name[$server_name] already exists,overwrite[o] or enter new server_name:" action
+		if [ ! $action ]
+		then
+			exit
+		fi
+
+		if [ $action == "o" ]
+		then
+			echo "" > $path$server_name
+		else
+			if [ $server_name == $action ]
+			then
+				echo "server_name already exists!"
+				exit
+			else
+				server_name=$action
+			fi
+		fi
+
+	fi
+
+    echo "server_name=$server_name" >> $path$server_name
+    echo "server_host=$server_host" >> $path$server_name
+    echo "server_port=$server_port" >> $path$server_name
+    echo "server_user=$server_user" >> $path$server_name
+    echo "server_password=$server_password" >> $path$server_name
+}
+
+ls(){
+	for file in $path*
+	do
+		while read line
+		do
+		    eval "$line"
+		done < $file
+	    echo "`basename $file`:$server_host"
+	    unset server_host
+	done
+}
+
+login(){
+	if [ ! -f $path$1 ]
+	then
+		echo "server_name[$1] does not exist"
+		exit
+	fi
+	while read line
+	do
+	    eval "$line"
+	done < $path$1
+	expect -c'
+	spawn ssh -p '$server_port' '$server_user'@'$server_host'
+	expect {
+		"*yes/no" { send "yes\r"; exp_continue}
+		"*password:" { send "'$server_password'\r" }
+	}
+	interact
+	'
+}
+
+delete(){
+	read -r -p "do you want to delete[y]?" action
+	if [ "$action"x == "y"x ]
+	then
+		rm -rf $path$1
+	fi
+	exit
+}
+
+case $command in
+  (add)
+     add
+     ;;
+  (ls)
+     ls
+     ;;
+  (rm)
+     delete $2
+     ;;
+  (*)
+     login $command
+     ;;
+esac
